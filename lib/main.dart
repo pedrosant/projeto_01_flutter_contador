ff528c713c1b59f194b6758145c5fcd4de4a65dd
Roberto Borges <pedro.borges@souunit.com.br>
2019-12-06 23:44:29 -0300

    Primeiro Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,66 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Aplicativo Contador",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+  runApp(MaterialApp(title: "Aplicativo Contador", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       children: <Widget>[
-        Text(
-          "Pessoa: 0",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        Image.asset(
+          "images/imagesApp1Flutter.png",
+          fit: BoxFit.cover,
+          height: 1000,
         ),
-        Row(
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FlatButton(
-              child:
-              Text("+1", style: TextStyle(fontSize: 40.0, color: Colors.white)
-              ),
-              onPressed: () {},
+            Text(
+              "Pessoa: 0",
+              style: TextStyle(color: Colors.teal, fontWeight: FontWeight.bold),
             ),
-            FlatButton(
-              child:
-              Text("-1", style: TextStyle(fontSize: 40.0, color: Colors.white)
-              ),
-              onPressed: () {},
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: FlatButton(
+                    child: Text("+1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.teal)),
+                    onPressed: () {
+                      debugPrint("+1");
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: FlatButton(
+                    child: Text("-1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.teal)),
+                    onPressed: () {
+                      debugPrint("-1");
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              "Pode Entrar !",
+              style: TextStyle(
+                  color: Colors.teal,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30),
             ),
           ],
         ),
-        Text(
-          "Pode Entrar !",
-          style: TextStyle(
-              color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
-        ),
       ],
-    ),
-  ));
+    );
+  }
 }
